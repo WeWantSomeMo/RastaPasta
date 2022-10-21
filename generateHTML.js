87462216f7c6afce63531b10203d45a705434b68
WeWantSomeMo <mason.oz5@yahoo.com>
2022-10-21 16:40:46 -0500

    Just needs foh

--- generateHTML.js
+++ generateHTML.js
@@ -1,45 +1,78 @@
 const Employee = require("./lib/Employee")
+const Foh = require("./lib/foh")
 const Kitchen = require("./lib/Kitchen")
 const Manager = require("./lib/Manager")
 
+
+const managerCard = Manager => {
+    console.log(`This is ln 6 of HTML.`, Manager)
+    return `
+        <div class="card">
+            <div class="card-header">
+                <h1 class="title"> Manager </h1>
+                <h2 class="role">${Manager.getRole()}🖥</h2>
+            </div>
+            <div class="card-body">
+                <ul class="list">
+                    <li class="info"> first name ${Manager.getfirstName()}</li>
+                    <li class="info"> last name ${Manager.getlastName()}</li>
+                    <li class="info"> Office#: ${Manager.getOfficeNum()}</li>
+                    <li class="info"> employee ID: ${Manager.getEmpId()}</li>
+                    <li class="info"> email: ${Manager.getEmail()}</li>
+                </ul>
+            </div>            
+        </div>
+         `
+}
+
+const kitchenCard = Kitchen => {
+    return `
+        <div class="card">
+            <div class="card-header">
+                <h1 class="title"> Kitchen </h1>
+                <h2 class="role">${Kitchen.getRole()}</h2>
+            </div>
+            <div class="card-body">
+                <ul class="list">
+                    <li class="info"> first name ${Kitchen.getfirstName()}</li>
+                    <li class="info"> last name ${Kitchen.getlastName()}</li>
+                    <li class="info"> safety: ${Kitchen.getSafety()}</li>
+                    <li class="info"> employee ID: ${Kitchen.getEmpId()}</li>
+                    <li class="info"> email: ${Kitchen.getEmail()}</li>
+                </ul>
+            </div>            
+        </div>
+         `
+}
+
+
 const formatHTML = Team => {
     console.log(`This is ln5 of HTML.`, Team)
-    const managerCard = Manager => {
-        console.log(`This is ln 6 of HTML.`, Manager)
-        return `
-            <div class="card">
-                <div class="card-header">
-                    <h1 class="title"> Manager </h1>
-                    <h2 class="role">${Manager.getRole()}🖥</h2>
-                </div>
-                <div class="card-body">
-                    <ul class="list">
-                        <li class="info"> first name ${Manager.getfirstName()}</li>
-                        <li class="info"> last name ${Manager.getlastName()}</li>
-                        <li class="info"> Office#: ${Manager.getOfficeNum()}</li>
-                        <li class="info"> employee ID: ${Manager.getEmpId()}</li>
-                        <li class="info"> email: ${Manager.getEmail()}</li>
-                    </ul>
-                </div>            
-            </div>
-             `
-    }
+    let htmlFile = []
+    let managerTeams = Team[0]
+    let fohTeams = Team[1]
+    let kitchenTeams = Team[2]
 
-    const htmlFile = []
-    htmlFile.push(Team
-
+    htmlFile.push(managerTeams
         .filter(Manager => Manager.getRole() === 'Manager')
         .map(Manager => managerCard(Manager))
     )
-        return htmlFile
+
+    // htmlFile.push(fohTeams
+    //     .filter(Manager => Manager.getRole() === 'Manager')
+    //     .map(Manager => managerCard(Manager))
+    // )
+
+    htmlFile.push(kitchenTeams
+        .filter(Kitchen => Kitchen.getRole() === 'Kitchen')
+        .map(Kitchen => kitchenCard(Kitchen))
+    )
+
+    return htmlFile
 }
 
 module.exports = Team => {
-    console.log('This is ln 37 HTML',Team)
-    console.log('This is Team 0', Team[0].length)
-    if (Team[0].length != 0) {
-        console.log('This is Manager', Team[0])
-    }
+
     return `
         <!DOCTYPE html>
         <html lang="en">
@@ -51,9 +84,6 @@ module.exports = Team => {
         </head>
         <body>
             <div> ${formatHTML(Team)}</div>
-            <div id="" class="columns">
-
-        </div>
         </body>
         </html>
     `
